0591ae83ea2b2ffb5fe9c5de41573045a9a30cdd
alpharabbit9 <alpharabbit74@gmail.com>
2025-09-22 12:50:08 +0600

    Added Signup point

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import { signUp } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.get('/signin', (req,res) =>{
-    res.send('Login route');
-})
+router.post('/signup',signUp)
 router.post('/login', (req,res) =>{
     res.send('Login route');
 })
